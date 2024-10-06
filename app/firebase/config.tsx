ec5b804221c14b248f6bd66c1dac1bf93f8dbd9a
Bhargav Shirin Nalamati <bnalamat@gitam.in>
2024-10-06 12:46:37 +0530

    Update config.tsx

--- app/firebase/config.tsx
+++ app/firebase/config.tsx
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
-    apiKey: "AIzaSyBs9h2ZKiNH47bjoUHMEfQvfHgjGMu0X3I",
-    authDomain: "visionx-b222f.firebaseapp.com",
-    projectId: "visionx-b222f",
-    storageBucket: "visionx-b222f.appspot.com",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
     messagingSenderId: "286056599115",
-    appId: "1:286056599115:web:0ee05dc61ded817dd187ce",
-    measurementId: "G-MCBC2V3DVV"
+    appId: "",
+    measurementId: ""
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
